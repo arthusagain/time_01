fd1d14cb2d909da64c77a7a13ef0194a379c468b
Arthur Ozorio <arthurlozorio@globo.com>
2020-01-31 20:25:28 -0300

    Movimentação

---
+++ GGJ/Assets/Scripts/Move.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public float movSpeed = 5.0f;
+    private Vector2 move;
+    private Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Movimento();
+    }
+
+    private void Movimento()
+    {
+        float movHon = Input.GetAxis("Horizontal");
+
+        Vector2 mov = new Vector2(movHon, 0);
+
+        rb.velocity = mov * movSpeed;
+    }
+}
+
