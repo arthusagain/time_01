fbab2cbcb4c5b80c8ae7b6d557b5bbabc51e2104
Arthur Ozorio <arthurlozorio@globo.com>
2020-01-31 20:39:49 -0300

    Câmera acompanha o player

---
+++ GGJ/Assets/Scripts/CamFollow.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public Transform player;
+    public float camSpeed = 1;
+    public Vector3 offset;
+
+    private void Update()
+    {
+        transform.position = player.position + offset;
+    }
+}
