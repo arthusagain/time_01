219d8f3ce5a6f58f555c670f9d8f28c3b15650ad
Arthur Ozorio <arthurlozorio@globo.com>
2020-02-01 08:57:20 -0300

    Jogador pode passar por npc após diálogo(completar fase)

--- GGJ/Assets/Scripts/DialogueTrigger.cs
+++ GGJ/Assets/Scripts/DialogueTrigger.cs
@@ -5,15 +5,24 @@ using UnityEngine;
 public class DialogueTrigger : MonoBehaviour
 {
     public Dialogue dialogue;
+    public bool falou = false;
 
+    private void Update()
+    {
+        if(falou)
+        {
+            this.gameObject.GetComponent<BoxCollider2D>().enabled = false;
+        }
+    }
     public void TriggerDialogue()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(dialogue);
+        falou = true;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.gameObject.CompareTag("Player"))
+        if (collision.gameObject.CompareTag("Player") && falou == false)
         {
             Debug.Log("oi");
             TriggerDialogue();
