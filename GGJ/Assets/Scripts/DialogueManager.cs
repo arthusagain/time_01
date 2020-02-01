a7a06c673d3cfa6727cf2eca0fefc38039c5be3a
Arthur Ozorio <arthurlozorio@globo.com>
2020-02-01 13:54:26 -0300

    Aparentemente tava bugado

--- GGJ/Assets/Scripts/DialogueManager.cs
+++ GGJ/Assets/Scripts/DialogueManager.cs
@@ -43,6 +43,7 @@ public class DialogueManager : MonoBehaviour
 
         foreach(string sentence in dialogue.sentences)
         {
+            Debug.Log(sentence);
             sentences.Enqueue(sentence);
         }
 
