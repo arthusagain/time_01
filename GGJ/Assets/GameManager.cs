cf7f8fbf6f1d780b2f4d9c5236c07bb364a412cd
Arthur Ozorio <arthurlozorio@globo.com>
2020-02-01 11:05:33 -0300

    barra de vida

---
+++ GGJ/Assets/GameManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public Image HPBar;
+    static public float HP;
+    private float HP_Total;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        HP_Total = HP;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        HPBar.fillAmount = HP / HP_Total;
+    }
+}
