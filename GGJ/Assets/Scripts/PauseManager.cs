92fd0f5967a5e8744bb7b5cb062e88c49c9318dc
arthusagain <arthus.james@hotmail.com>
2020-02-02 11:12:32 -0300

    instruções do puzzle e pausa (ainda sem botão)

---
+++ GGJ/Assets/Scripts/PauseManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    public void Pause()
+    {
+        Time.timeScale = 0;
+        
+    }
+    public void UnPause()
+    {
+        Time.timeScale = 1;
+    }
+}
